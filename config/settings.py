6b5f6688e2a15bc3d62885d0ccfe8f47f4675db5
Harsh Singh <student.harsh11@gmail.com>
2026-03-22 22:28:19 +0530

    update installed app

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     
     # Third-Party
     'rest_framework',
